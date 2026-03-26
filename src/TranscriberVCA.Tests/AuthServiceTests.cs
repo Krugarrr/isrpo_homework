6d6738a8bd56f266d83776156e68343d7008b5a9
krugarrr <ttteeework@gmail.com>
2026-03-26 14:08:46 +0300

    feat: CI/CD

---
+++ src/TranscriberVCA.Tests/AuthServiceTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using TranscriberVCA.Generated.Models;
+using TranscriberVCA.Generated.Services;
+using Xunit;
+
+namespace TranscriberVCA.Tests;
+
+public class AuthServiceTests
+{
+    private readonly AuthService _authService;
+
+    public AuthServiceTests()
+    {
+        var logger = new Mock<ILogger<AuthService>>();
+        _authService = new AuthService(logger.Object);
+    }
+
+    [Fact]
+    public void Register_NewUser_ReturnsTokenAndUsername()
+    {
+        var request = new RegisterRequest { Username = "sanechek", Password = "password123" };
+
+        var result = _authService.Register(request);
+
+        result.Should().NotBeNull();
+        result.Token.Should().NotBeNullOrEmpty();
+        result.Username.Should().Be("sanechek");
+    }
+
+    [Fact]
+    public void Register_DuplicateUser_ThrowsException()
+    {
+        var request = new RegisterRequest { Username = "gigachad", Password = "password123" };
+        _authService.Register(request);
+
+        var act = () => _authService.Register(request);
+
+        act.Should().Throw<InvalidOperationException>()
+            .WithMessage("User already exists");
+    }
+
+    [Fact]
+    public void Login_ValidCredentials_ReturnsToken()
+    {
+        var registerRequest = new RegisterRequest { Username = "eminchik", Password = "password123" };
+        _authService.Register(registerRequest);
+
+        var loginRequest = new LoginRequest { Username = "eminchik", Password = "password123" };
+        var result = _authService.Login(loginRequest);
+
+        result.Should().NotBeNull();
+        result.Token.Should().NotBeNullOrEmpty();
+        result.Username.Should().Be("eminchik");
+    }
+
+    [Fact]
+    public void Login_WrongPassword_ThrowsUnauthorized()
+    {
+        var registerRequest = new RegisterRequest { Username = "maximche", Password = "correct" };
+        _authService.Register(registerRequest);
+
+        var loginRequest = new LoginRequest { Username = "maximche", Password = "incorrect" };
+        var act = () => _authService.Login(loginRequest);
+
+        act.Should().Throw<UnauthorizedAccessException>()
+            .WithMessage("Invalid credentials");
+    }
+
+    [Fact]
+    public void Login_NonExistentUser_ThrowsUnauthorized()
+    {
+        var loginRequest = new LoginRequest { Username = "skibidist", Password = "password" };
+        var act = () => _authService.Login(loginRequest);
+
+        act.Should().Throw<UnauthorizedAccessException>()
+            .WithMessage("Invalid credentials");
+    }
+
+    [Fact]
+    public void ValidateToken_ValidToken_ReturnsUsername()
+    {
+        var registerRequest = new RegisterRequest { Username = "kilagen", Password = "password123" };
+        var registerResult = _authService.Register(registerRequest);
+
+        var username = _authService.ValidateToken(registerResult.Token);
+
+        username.Should().Be("kilagen");
+    }
+
+    [Fact]
+    public void ValidateToken_InvalidToken_ReturnsNull()
+    {
+        var result = _authService.ValidateToken("invalid.token.here");
+
+        result.Should().BeNull();
+    }
+}
---
+++ src/TranscriberVCA.Tests/TranscriptionServiceTests.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using TranscriberVCA.Generated.Models;
+using TranscriberVCA.Generated.Services;
+using Xunit;
+
+namespace TranscriberVCA.Tests;
+
+public class TranscriptionServiceTests
+{
+    private readonly TranscriptionService _service;
+
+    public TranscriptionServiceTests()
+    {
+        var logger = new Mock<ILogger<TranscriptionService>>();
+        _service = new TranscriptionService(logger.Object);
+    }
+
+    [Fact]
+    public void StartTranscription_ReturnsResultWithId()
+    {
+        var request = new TranscribeRequest
+        {
+            YoutubeUrl = "https://youtube.com/watch?v=test1",
+            Model = TranscribeRequest.ModelEnum.BaseEnum,
+            Format = TranscribeRequest.FormatEnum.TxtEnum
+        };
+
+        var result = _service.StartTranscription("olyakub", request);
+
+        result.Should().NotBeNull();
+        result.Id.Should().NotBeNullOrEmpty();
+        result.YoutubeUrl.Should().Be("https://youtube.com/watch?v=test1");
+        result.Status.Should().Be(TranscriptionResult.StatusEnum.TranscribingEnum);
+    }
+
+    [Fact]
+    public void StartTranscription_SetsCorrectModelAndFormat()
+    {
+        var request = new TranscribeRequest
+        {
+            YoutubeUrl = "https://youtube.com/watch?v=test2",
+            Model = TranscribeRequest.ModelEnum.LargeEnum,
+            Format = TranscribeRequest.FormatEnum.JsonEnum
+        };
+
+        var result = _service.StartTranscription("olyakub", request);
+
+        result.Model.Should().Be(TranscriptionResult.ModelEnum.LargeEnum);
+        result.Format.Should().Be(TranscriptionResult.FormatEnum.JsonEnum);
+    }
+
+    [Fact]
+    public void GetTranscription_ExistingId_ReturnsResult()
+    {
+        var request = new TranscribeRequest
+        {
+            YoutubeUrl = "https://youtube.com/watch?v=test3",
+            Model = TranscribeRequest.ModelEnum.BaseEnum,
+            Format = TranscribeRequest.FormatEnum.TxtEnum
+        };
+        var started = _service.StartTranscription("olyakub", request);
+
+        var result = _service.GetTranscription("olyakub", started.Id);
+
+        result.Should().NotBeNull();
+        result!.Id.Should().Be(started.Id);
+    }
+
+    [Fact]
+    public void GetTranscription_NonExistentId_ReturnsNull()
+    {
+        var result = _service.GetTranscription("olyakub", "nonexistent");
+
+        result.Should().BeNull();
+    }
+
+    [Fact]
+    public void GetTranscription_WrongUser_ReturnsNull()
+    {
+        var request = new TranscribeRequest
+        {
+            YoutubeUrl = "https://youtube.com/watch?v=test4",
+            Model = TranscribeRequest.ModelEnum.BaseEnum,
+            Format = TranscribeRequest.FormatEnum.TxtEnum
+        };
+        var started = _service.StartTranscription("olyakub", request);
+
+        var result = _service.GetTranscription("sashakrug", started.Id);
+
+        result.Should().BeNull();
+    }
+
+    [Fact]
+    public void GetHistory_NoTranscriptions_ReturnsEmptyList()
+    {
+        var result = _service.GetHistory("deadinside", 10, 0);
+
+        result.Should().NotBeNull();
+        result.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void GetHistory_WithTranscriptions_ReturnsCorrectCount()
+    {
+        var request = new TranscribeRequest
+        {
+            YoutubeUrl = "https://youtube.com/watch?v=test5",
+            Model = TranscribeRequest.ModelEnum.BaseEnum,
+            Format = TranscribeRequest.FormatEnum.TxtEnum
+        };
+
+        _service.StartTranscription("skibidimaniac", request);
+        _service.StartTranscription("skibidimaniac", request);
+        _service.StartTranscription("skibidimaniac", request);
+
+        var result = _service.GetHistory("skibidimaniac", 10, 0);
+
+        result.Should().HaveCount(3);
+    }
+
+    [Fact]
+    public void GetHistory_WithLimitAndOffset_ReturnsCorrectSlice()
+    {
+        var request = new TranscribeRequest
+        {
+            YoutubeUrl = "https://youtube.com/watch?v=test6",
+            Model = TranscribeRequest.ModelEnum.BaseEnum,
+            Format = TranscribeRequest.FormatEnum.TxtEnum
+        };
+
+        for (int i = 0; i < 5; i++)
+            _service.StartTranscription("chromekiller", request);
+
+        var result = _service.GetHistory("chromekiller", 2, 1);
+
+        result.Should().HaveCount(2);
+    }
+
+    [Fact]
+    public async Task StartTranscription_CompletesAfterProcessing()
+    {
+        var request = new TranscribeRequest
+        {
+            YoutubeUrl = "https://youtube.com/watch?v=test7",
+            Model = TranscribeRequest.ModelEnum.BaseEnum,
+            Format = TranscribeRequest.FormatEnum.TxtEnum
+        };
+
+        var started = _service.StartTranscription("blablabla", request);
+
+        await Task.Delay(6000);
+
+        var result = _service.GetTranscription("blablabla", started.Id);
+
+        result.Should().NotBeNull();
+        result!.Status.Should().Be(TranscriptionResult.StatusEnum.CompletedEnum);
+    }
+}
